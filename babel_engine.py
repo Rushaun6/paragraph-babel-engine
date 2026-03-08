8e40945899ecf98c99fb5eb368d1b786ee614a4f
Rushaun6 <166856723+Rushaun6@users.noreply.github.com>
2026-03-07 23:05:23 -0500

    Create babel_engine.py

---
+++ babel_engine.py
@@ -0,0 +1,45 @@
+CHARSET = "abcdefghijklmnopqrstuvwxyz0123456789 .,?!'"
+BASE = len(CHARSET)
+
+KEY_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+KEY_BASE = len(KEY_CHARS)
+
+
+def encode_to_number(text):
+    num = 0
+    for char in text:
+        value = CHARSET.index(char)
+        num = num * BASE + value
+    return num
+
+
+def number_to_key(num):
+    key = ""
+    while num > 0:
+        num, rem = divmod(num, KEY_BASE)
+        key = KEY_CHARS[rem] + key
+    return key
+
+
+def key_to_number(key):
+    num = 0
+    for char in key:
+        value = KEY_CHARS.index(char)
+        num = num * KEY_BASE + value
+    return num
+
+
+def decode_from_number(num):
+    text = ""
+    while num > 0:
+        num, rem = divmod(num, BASE)
+        text = CHARSET[rem] + text
+    return text
+
+
+def encode(text):
+    return number_to_key(encode_to_number(text))
+
+
+def decode(key):
+    return decode_from_number(key_to_number(key))
